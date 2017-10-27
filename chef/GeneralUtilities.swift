88013c23f07ca88103d323fbdf1ca32bb2ef10b0
Diwakar Kamboj <primekrieger@gmail.com>
2017-10-28 02:11:38 +0530

    Update getAlarmDetailsString method

--- chef/GeneralUtilities.swift
+++ chef/GeneralUtilities.swift
@@ -22,48 +22,31 @@ class GeneralUtilities {
         let alarmTimeString = dateFormatter.string(from: alarmTime!)
         
         var repeatDays = String()
-        
-        var repetitionCount = 0
-        var lastRepetitionDay: String!
+        var lastActiveRepetitionDay: String?
+        var lastInactiveRepetitionDay: String?
+        let inactiveRepetitionDaySymbol = "•"
         
         for i in 0..<alarm.shouldRepeatOnDays.count {
             if alarm.shouldRepeatOnDays[i].value {
-                repetitionCount += 1
-                switch i {
-                case 0:
-                    repeatDays.append("Su ")
-                    lastRepetitionDay = "Sunday"
-                case 1:
-                    repeatDays.append("Mo ")
-                    lastRepetitionDay = "Monday"
-                case 2:
-                    repeatDays.append("Tu ")
-                    lastRepetitionDay = "Tuesday"
-                case 3:
-                    repeatDays.append("We ")
-                    lastRepetitionDay = "Wednesday"
-                case 4:
-                    repeatDays.append("Th ")
-                    lastRepetitionDay = "Thursday"
-                case 5:
-                    repeatDays.append("Fr ")
-                    lastRepetitionDay = "Friday"
-                case 6:
-                    repeatDays.append("Sa ")
-                    lastRepetitionDay = "Saturday"
-                default:
-                    break
-                }
+                repeatDays.append(dateFormatter.veryShortWeekdaySymbols[i])
+                lastActiveRepetitionDay = dateFormatter.weekdaySymbols[i]
+            } else {
+                repeatDays.append(inactiveRepetitionDaySymbol)
+                lastInactiveRepetitionDay = dateFormatter.weekdaySymbols[i]
             }
         }
         
-        if repetitionCount == 1 {
-            repeatDays = "Every " + lastRepetitionDay
-        } else if repeatDays == "Su Mo Tu We Th Fr Sa " {
+        let repeatDaysTrimmed = repeatDays.replacingOccurrences(of: inactiveRepetitionDaySymbol, with: "")
+        
+        if repeatDaysTrimmed.count == 1 {
+            repeatDays = "Every " + lastActiveRepetitionDay!
+        } else if repeatDaysTrimmed.count == 6 {
+            repeatDays = "Except " + lastInactiveRepetitionDay!
+        } else if repeatDaysTrimmed == "SMTWTFS" {
             repeatDays = "Every day"
-        } else if repeatDays == "Su Sa " {
+        } else if repeatDaysTrimmed == "SS" {
             repeatDays = "Weekends"
-        } else if repeatDays == "Mo Tu We Th Fr " {
+        } else if repeatDaysTrimmed == "MTWTF" {
             repeatDays = "Weekdays"
         }
         
